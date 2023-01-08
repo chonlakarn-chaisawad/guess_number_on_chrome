107389b590e4fc441fae0dd88c019ff2da4e013a
Chonlakarn Chaisawad <chaisawad_c@silpakorn.edu>
2023-01-08 09:58:50 +0700

    Initial commit

---
+++ lib/page/game/game_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GamePage());
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Guess the Number'),
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // ให้อยู่ตรงกลาง
+                children: [
+              Icon(Icons.person, size: 200, color: Colors.purpleAccent),
+              Text('Guess the number between 1 and 100'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, // ให้อยู่ตรงกลาง
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                        ElevatedButton(onPressed: () {}, child: Text('Guess')),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Guess'))
+                ],
+              )
+            ])));
+  }
+}
